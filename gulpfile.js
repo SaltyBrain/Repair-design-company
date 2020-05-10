e1df25bcaa2bc791063d33769c76179ca9d4b890
SaltyBr <SaltyBrain@yandex.ru>
2020-05-10 22:43:18 +0300

    summary

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('Hello world');
@@ -13,4 +15,11 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('mincss', function () {
+  gulp.src('src/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist'));
 });
