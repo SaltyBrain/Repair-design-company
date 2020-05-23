6d45210d55bafc7240cf9f62dc5e8b6a7da622db
SaltyBr <SaltyBrain@yandex.ru>
2020-05-23 16:44:27 +0300

    add main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+   const modal = document.querySelector('.modal');
+   const modalButton = document.querySelectorAll('[data-toggle = modal]');
+   const closeBtn = document.querySelector('.modal__close');
+   const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+   }
+
+   //adding modal--visible for click
+  modalButton.forEach (element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+});
